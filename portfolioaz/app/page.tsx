379a3689c328845f21b427b7e8d66a59022104b3
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2023-11-02 16:02:35 +0100

    added circle

--- portfolioaz/app/page.tsx
+++ portfolioaz/app/page.tsx
@@ -9,13 +9,13 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
         {/* Header */}
         <Header />
 
         {/* Hero  */}
 
-        <section id='hero'>
+        <section id='hero' className='snap-center'>
           <Hero />
 
         </section>
--- portfolioaz/components/Hero.tsx
+++ portfolioaz/components/Hero.tsx
@@ -1,10 +1,30 @@
+"use client"
 import React from 'react'
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
+import BackgroundCircles from './BackgroundCircles'
+
 
 type Props = {}
 
 export default function Hero({}: Props) {
-  return <div>
-    
-  </div>
+    const [text, count] = useTypewriter({
+        words: [
+            "Hi, The name's Alex",
+            "Guy-who-loves-Coffee.tsx",
+            "<ButLovesToCodeMore>",
+        ],
+        loop: true,
+        delaySpeed:2000,
+    });
+
+  return (
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+        <BackgroundCircles />
+        <h1>
+            <span>{text}</span>
+            <Cursor cursorColor='#F7AB0A'/>
+        </h1>
+    </div>
+    )
   
 }
